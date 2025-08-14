f524dc3659893abdd63baf2c37738f16094dc01a
PhoevosA <3015850A@student.gla.ac.uk>
2025-08-14 12:59:17 +0100

    Update generate_rst.py

--- generate_rst.py
+++ generate_rst.py
@@ -15,7 +15,7 @@ for path in folder_paths:
     for python_file in python_files:
         module_name = python_file.split(".")[0]
         if module_name == "main":
-          module_name = page_name + "-" + module_name
+          module_name = path.split("/")[-1] + "." + module_name
         f.write(f"\n{python_file}\n")
         f.write("^" * len(python_file) + "\n")
         f.write("\n")
