bad647e9444aad6260e79249a531a6ef0b2820f3
PhoevosA <3015850A@student.gla.ac.uk>
2025-08-14 11:36:00 +0100

    Create generate_rst.py

---
+++ generate_rst.py
@@ -0,0 +1,27 @@
+import os
+
+folder_paths = ['splice-api/app','splice-cli/src/splice_cli','splice-lib/src/splice_lib']
+
+for path in folder_paths:
+  python_files = [f for f in os.listdir(path) if f.endswith('.py') and f != "__init__.py"]
+  page_name = path.split("/")[0]
+  page_name = page_name.split("-")[1].capitalize()
+  rst_filename = f"{page_name}.rst"
+  with open(os.path.join("docs/source", rst_filename), 'w') as f:
+  
+  
+    f.write(f"{page_name}\n")
+    f.write("=" * len(page_name))
+    for python_file in python_files:
+        module_name = python_file.split(".")[0]
+        f.write(f"\n{python_file}\n")
+        f.write("^" * len(python_file) + "\n")
+        f.write("\n")
+        f.write(f".. automodule:: {module_name}\n")
+        f.write("   :member-order: groupwise\n")
+        f.write("   :members:\n")
+        f.write("   :undoc-members:\n")
+        f.write("   :show-inheritance:\n")
+        f.write("   :noindex:\n")
+
+  
