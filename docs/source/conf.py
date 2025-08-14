54939724147d98ed177a1889c2fc29b6662b3e5e
PhoevosA <3015850A@student.gla.ac.uk>
2025-08-14 11:56:24 +0100

    Create conf.py

---
+++ docs/source/conf.py
@@ -0,0 +1,46 @@
+# Configuration file for the Sphinx documentation builder.
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../../splice-api/app'))
+sys.path.insert(0, os.path.abspath('../../splice-cli/src/splice_cli'))
+sys.path.insert(0, os.path.abspath('../../splice-lib/src/splice_lib'))
+sys.path.insert(0, os.path.abspath('../../splice-pipeline'))
+sys.path.insert(0, os.path.abspath('../../splice-tests'))
+sys.path.insert(0, os.path.abspath('../../splice-ui'))
+# -- Project information
+
+project = 'SPLICE test'
+copyright = '2025, Me'
+author = 'Me'
+
+release = '0.1'
+version = '0.1.0'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
+
+autosummary_generate = True
